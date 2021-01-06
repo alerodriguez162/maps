45f0f4e280c30437c912c297ff09a84730060dfe
alerodriguez162 <alerodriguez162@gmail.com>
2021-01-05 19:32:23 -0600

    Update defaultConfig.js

--- src/static/defaultConfig.js
+++ src/static/defaultConfig.js
@@ -1,48 +1,48 @@
-import { IN_PROGRESS } from './flightStates'
+import { IN_PROGRESS } from "./flightStates";
 
 const DefaultConfig = {
-  mapContainerId: 'map',
+  mapContainerId: "map",
   linesArc: -0.7,
   globalFlightsState: IN_PROGRESS,
   colors: {
-    land: '#383747',
-    background: '#504F64',
-    aircrafts: '#000000',
-    lines: '#000000',
-    cities: '#000000'
+    land: "#D9E4FF",
+    background: "F2F6FF",
+    aircrafts: "#5786CC",
+    lines: "#00000000",
+    cities: "#000000",
   },
   zoomedContinent: null,
   forceUpdate: false,
   showMarkers: false,
   dataToShowOnMarkers: [
     {
-      id: 'name',
-      text: 'Flight'
+      id: "name",
+      text: "Flight",
     },
     {
-      id: 'origin.city',
-      text: 'Origin'
+      id: "origin.city",
+      text: "Origin",
     },
     {
-      id: 'destination.city',
-      text: 'Destination'
+      id: "destination.city",
+      text: "Destination",
     },
     {
-      id: 'state',
-      text: 'State',
+      id: "state",
+      text: "State",
       additional: {
-        0: 'Pending to start',
-        1: 'Flying',
-        2: 'Completed'
-      }
-    }
+        0: "Pending to start",
+        1: "Flying",
+        2: "Completed",
+      },
+    },
   ],
   animation: {
     enabled: true,
     flip: false,
     loop: true,
     shouldAnimateFlyingState: true,
-    duration: 8.5
+    duration: 8.5,
   },
   zoom: {
     initialLevel: 1,
@@ -51,20 +51,20 @@ const DefaultConfig = {
     initialCenter: {
       latitude: null,
       longitude: null,
-      level: null
+      level: null,
     },
     homeButtonEnabled: false,
     buttonFillAlpha: 0.7,
-    buttonFillColor: '#E6E6E6',
+    buttonFillColor: "#E6E6E6",
     controlsPosition: {
       top: 20,
-      right: 20
-    }
+      right: 20,
+    },
   },
   texts: {
     labelsFontSize: 8,
-    markersFontSize: 14
-  }
-}
+    markersFontSize: 14,
+  },
+};
 
-export default DefaultConfig
+export default DefaultConfig;
